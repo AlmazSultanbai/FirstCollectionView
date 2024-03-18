00bebf78200a209556dfda7c9830c181bc83767d
Almaz Sultanbai <aldeco312@gmail.com>
2024-03-18 12:36:38 +0600

    Initial Commit

---
+++ FirstCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstCollectionView
+//
+//  Created by Sultanbai Almaz on 18/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
