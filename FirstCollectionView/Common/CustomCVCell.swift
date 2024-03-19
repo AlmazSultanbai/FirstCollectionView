ec38dff2f9af3a9317cfdcfeceb01fa9150f197c
Almaz Sultanbai <aldeco312@gmail.com>
2024-03-19 10:39:18 +0600

    commit2 local

--- FirstCollectionView/Common/CustomCVCell.swift
+++ FirstCollectionView/Common/CustomCVCell.swift
@@ -7,21 +7,57 @@
 
 import UIKit
 
-class CustomCollectionViewCell: UICollectionViewCell {
+class CustomCVCell: UICollectionViewCell {
     
     private let indexLabel: UILabel = {
         let view = UILabel()
         view.text = "Hello"
         view.textColor = UIColor.blue
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.textAlignment = .center
+        return view
+    }()
+    
+    private let photoIV: UIImageView = {
+        let view = UIImageView()
+        view.contentMode = .scaleAspectFit
+        view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
     
     override init(frame: CGRect) {
-        super.init(frame: <#T##CGRect#>)
+        super.init(frame: frame)
+        contentView.backgroundColor = .green
+        contentView.addSubview(indexLabel)
+        contentView.addSubview(photoIV)
+        setupConstraints()
+    }
+    
+   
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            indexLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 5),
+            indexLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 5),
+            indexLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -5),
+            indexLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -5),
+            
+            photoIV.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            photoIV.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            photoIV.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+            photoIV.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 10)
+        ])
+    }
+    
+    func setIndex(_ index: String) {
+        indexLabel.text = index
+    }
+    
+    func setImage(image: String) {
+        photoIV.image = UIImage(named: "car1")
+        
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
 }
--- FirstCollectionView/Common/ViewController.swift
+++ FirstCollectionView/Common/ViewController.swift
@@ -8,12 +8,71 @@
 import UIKit
 
 class ViewController: UIViewController {
+   
+    private let simpleCollectionView: UICollectionView = {
+        
+        let cvFlayout = UICollectionViewFlowLayout()  //определяет разметки между сeкцией и  ячейками CollectionView
+        cvFlayout.itemSize = CGSize(width: (UIScreen.main.bounds.width / 4)  - 20 , height: 80)
+        
+        cvFlayout.minimumLineSpacing = 20// отступ между колоннами
+        
+        cvFlayout.minimumInteritemSpacing = 10 //отступ между рядами
+        cvFlayout.sectionInset = UIEdgeInsets(top: 50, left: 10, bottom: 50, right: 10)
+        cvFlayout.scrollDirection  = .vertical
+//        cVFlayout.sectionInset.top = 30
+//        cVFlayout.sectionInset.bottom = 30
+//        cVFlayout.sectionInset.left = 10
+//        cVFlayout.sectionInset.right = 10
+        cvFlayout.collectionView?.layer.borderColor = UIColor.white.cgColor
+        
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: cvFlayout)
+        cv.backgroundColor = .white
+        cv.layer.borderColor = UIColor.black.cgColor
+        cv.layer.cornerRadius = 15
+        cv.layer.borderWidth = 6
+        return cv
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        view.addSubview(simpleCollectionView)
+        simpleCollectionView.dataSource = self
+        simpleCollectionView.register(CustomCVCell.self, forCellWithReuseIdentifier: "Cell")
+        setupConstraints()
     }
+    
+    func setupConstraints(){
+        simpleCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            
+            simpleCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            simpleCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            simpleCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            simpleCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50)
+            
+        ])
+        
+    }
+    
 
 
 }
-
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        100
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+//        cell.backgroundColor = .systemGray
+//        return cell
+        let cell  = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as? CustomCVCell
+        //cell?.setIndex(String(indexPath.row))
+        cell?.setImage(image: "car1")
+        
+        return cell ?? UICollectionViewCell()
+    
+    }
+}
